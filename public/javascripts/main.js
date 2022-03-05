8b88cccad47d6cad3d8ff6f1df94fdae86af0dbd
Thiago da Silva <thiagosilva@uni.minerva.edu>
2022-03-05 15:45:10 +0000

    Initial commit - Implements same logic as other Metamask repo with Expressjs

---
+++ public/javascripts/main.js
@@ -0,0 +1,21 @@
+const greetingParagraph = document.querySelector('#greeting');
+const warningParagraph = document.querySelector('#warning');
+const ethereumButton = document.querySelector('.enableEthereumButton');
+
+let hasWallet = (typeof ethereum !== 'undefined') ? true : false;
+
+ethereumButton.addEventListener('click', () => {
+  
+  (async () => {
+    if(hasWallet){
+    //Get metamask account
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    
+    //add it to greeting paragraph
+    greetingParagraph.innerHTML = `Hello ${accounts[0]}`
+    }
+    else{
+      warningParagraph.innerHTML = 'Please install Metamask to use this functionality';
+    }
+  })();
+});
